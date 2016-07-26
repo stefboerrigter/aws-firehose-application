981ded57b0e2f53be11b99a096232448746ead3d
Stef Boerrigter <stef.boerrigter@bench.com>
2016-07-26 08:50:13 +0200

    adding cross library and application and output to archive for testing

---
+++ crossbuild/buildOutput.sh
@@ -0,0 +1,30 @@
+-- The C compiler identification is GNU 4.9.2
+-- The CXX compiler identification is GNU 4.9.2
+-- Check for working C compiler: /usr/local/oecore-x86_64/sysroots/x86_64-oesdk-linux/usr/bin/arm-oe-linux-gnueabi/arm-oe-linux-gnueabi-gcc
+-- Check for working C compiler: /usr/local/oecore-x86_64/sysroots/x86_64-oesdk-linux/usr/bin/arm-oe-linux-gnueabi/arm-oe-linux-gnueabi-gcc -- works
+-- Detecting C compiler ABI info
+-- Detecting C compiler ABI info - done
+-- Detecting C compile features
+-- Detecting C compile features - done
+-- Check for working CXX compiler: /usr/local/oecore-x86_64/sysroots/x86_64-oesdk-linux/usr/bin/arm-oe-linux-gnueabi/arm-oe-linux-gnueabi-g++
+-- Check for working CXX compiler: /usr/local/oecore-x86_64/sysroots/x86_64-oesdk-linux/usr/bin/arm-oe-linux-gnueabi/arm-oe-linux-gnueabi-g++ -- works
+-- Detecting CXX compiler ABI info
+-- Detecting CXX compiler ABI info - done
+-- Detecting CXX compile features
+-- Detecting CXX compile features - done
+-- /usr/local/oecore-x86_64/sysroots/cortexa8hf-vfp-neon-oe-linux-gnueabi/usr/lib/libaws-cpp-sdk-firehose.so
+-- /usr/local/oecore-x86_64/sysroots/cortexa8hf-vfp-neon-oe-linux-gnueabi/usr/lib/libaws-cpp-sdk-cognito-identity.so
+-- /usr/local/oecore-x86_64/sysroots/cortexa8hf-vfp-neon-oe-linux-gnueabi/usr/lib/libaws-cpp-sdk-access-management.so
+-- /usr/local/oecore-x86_64/sysroots/cortexa8hf-vfp-neon-oe-linux-gnueabi/usr/lib/libaws-cpp-sdk-core.so
+-- /usr/local/oecore-x86_64/sysroots/cortexa8hf-vfp-neon-oe-linux-gnueabi/usr/lib/libaws-cpp-sdk-iam.so
+-- Configuring done
+-- Generating done
+-- Build files have been written to: /home/boerrs/benchmark/aron/SQS_Inv/aws-firehose-lib/crossbuild
+Scanning dependencies of target aws-firehose
+[ 50%] Building CXX object CMakeFiles/aws-firehose.dir/FirehoseLibraryClient.cpp.o
+Linking CXX static library libaws-firehose.a
+[ 50%] Built target aws-firehose
+Scanning dependencies of target firehose-application
+[100%] Building CXX object CMakeFiles/firehose-application.dir/firehoseTests.cpp.o
+Linking CXX executable firehose-application
+[100%] Built target firehose-application
