c4b7fa701c34e4f5c97e1147637e89dbb8f2ccf2
Stef Boerrigter <stef.boerrigter@bench.com>
2016-07-26 09:46:15 +0200

    copy automatically cross output to sysroot

--- crossbuild/build.sh
+++ crossbuild/build.sh
@@ -1,3 +1,9 @@
 source /usr/local/oecore-x86_64/environment-setup-cortexa8hf-vfp-neon-oe-linux-gnueabi
 cmake ../
 make -j4
+
+read -p "Enter sysroot dir (default: /usr/local/oecore-x86_64):" dir
+dir=${dir:-/usr/local/oecore-x86_64}
+
+sudo cp libaws-firehose.a $dir/sysroots/cortexa8hf-vfp-neon-oe-linux-gnueabi/usr/lib/ -v
+sudo cp ../FirehoseLibraryClient.h $dir/sysroots/cortexa8hf-vfp-neon-oe-linux-gnueabi/usr/include/ -v
